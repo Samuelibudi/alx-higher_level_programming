9f3bce7094f306540653c93dda38397a3cad2ecc
Samuelibudi <ibudisamuel@gmail.com>
2022-12-01 12:23:32 -0800

    Fixing errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,9 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    if ((len(sys.argv) - 1) != 3):
+    num = len(sys.argv) - 1
+
+    if (num != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     op = sys.argv[2]
 
     if (op != '+' and op != '-' and op != '*' and op != '/'):
-        print("Unknown operator. available operators:+, -, * and /")
+        print("Unknown operator. available operators: +, -, * and /")
         sys.exit(1)
 
     if op == '+':
---
+++ 0x02-python-import_modules/mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+nargs = len(sys.argv)
+print(nargs)
+i = 0
+while i < nargs:
+    print(sys.argv[i])
+    i += 1
