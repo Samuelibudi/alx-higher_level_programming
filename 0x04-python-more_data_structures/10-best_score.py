b7a68c213a60a858f4307d0344dd69cef6758841
Samuelibudi <ibudisamuel@gmail.com>
2022-12-07 04:42:42 -0800

    fixing errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    keys = list(a_dictionary.keys())
-
-    big_int = 0
-
-    for i in keys:
-        if big_int < a_dictionary[i]:
-            big_int = a_dictionary[i]
-
-    if big_int != 0:
-        return big_int
-    else:
+    if not a_dictionary:
         return None
+
+    return (max(a_dictionary, key=a_dictionary.get))
