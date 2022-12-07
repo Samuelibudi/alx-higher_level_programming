52874fa9d15d25b7b5645d5d53e6e6939666592c
Samuelibudi <ibudisamuel@gmail.com>
2022-12-07 04:05:05 -0800

    Addig files

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    a_dictionary = dict(sorted(a_dictionary.items()))
+
+    for k, v in a_dictionary.items():
+        print(k, ': ', v)
