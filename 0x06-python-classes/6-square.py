6f778168f53790d571d362f9f5b67a43d7a6c6e5
Samuelibudi <ibudisamuel@gmail.com>
2022-12-20 02:01:37 -0800

    Fixing errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,15 +6,11 @@
 class Square:
     """Class that defines a square by its size"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Method to initialise square object"""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.size = size
+        self.position = position
 
     def area(self):
         """ Method that returns the square area of an object"""
