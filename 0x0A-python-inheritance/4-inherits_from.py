ab678011785f4a264943b73070e277b2431f6667
Samuelibudi <ibudisamuel@gmail.com>
2023-01-09 04:52:32 -0800

    Fixing whitespace error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module defines function that checks if an object 
+"""Module defines function that checks if an object
     is an instance of a class that inherited(directly
     or indirectly) from the specicified class."""
 
