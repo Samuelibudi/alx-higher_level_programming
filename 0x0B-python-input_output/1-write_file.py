d14de5dd987df10928bfb70c15cf66cd02a121ef
Samuelibudi <ibudisamuel@gmail.com>
2023-01-10 19:46:23 +0000

    Adding file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
+""" Module that contains a function that writes to a text file
 """
 
 
-def number_of_lines(filename="", text=""):
-    """ Function that reads from a file and prints its number of lines """
-    
+def write_file(filename="", text=""):
+    """ Function that writes to a text file """
+
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
