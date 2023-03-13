d68c40b88da3487029627fedd5d9d50f9c8360b9
Samuelibudi <ibudisamuel@gmail.com>
2023-03-13 07:16:55 -0700

    Adding files

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
