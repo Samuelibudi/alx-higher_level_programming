cc272e9aae2508daebdd7f0e5a01f5fcb7417f66
Samuelibudi <ibudisamuel@gmail.com>
2023-03-13 05:08:05 -0700

    Adding files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myVar = 'JavaScript is amazing';
+const myVar1 = 'Python is cool';
+const myVar2 = 'C is fun';
+console.log(myVar2);
+console.log(myVar1);
+console.log(myVar);
