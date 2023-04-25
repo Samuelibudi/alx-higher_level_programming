84f312654ccb20b365ebc2cc6bbea33ee79168a3
Samuelibudi <ibudisamuel@gmail.com>
2023-04-25 17:54:36 +0000

    Adding file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const movie = JSON.parse(body);
-    if (movie.episode_id === movieId) {
-      console.log(movie.title);
-    }
+const id = process.argv[2];
+request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
 });
