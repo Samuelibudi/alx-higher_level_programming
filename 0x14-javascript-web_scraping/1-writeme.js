86613b31056a2e71ee2e29d97f6e9fd9cfbec1ab
Samuelibudi <ibudisamuel@gmail.com>
2023-04-25 17:21:21 +0000

    Adding file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { promisify } = require('util');
+
+const writeFile = promisify(fs.writeFile);
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+async function writeToFile () {
+  try {
+    await writeFile(filePath, content, 'utf-8');
+    console.log(`Successfully wrote '${content}' to '${filePath}'`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+writeToFile();
